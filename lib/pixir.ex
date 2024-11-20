c5ee82777c21f290f8616bbf56e3eff421944a34
Adam Ulrich <adamo.ulrich@gmail.com>
2024-11-20 14:16:38 +0100

    initial commit

---
+++ lib/pixir.ex
@@ -0,0 +1,18 @@
+defmodule Pixir do
+  @moduledoc """
+  Documentation for `Pixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pixir_test.exs
@@ -0,0 +1,8 @@
+defmodule PixirTest do
+  use ExUnit.Case
+  doctest Pixir
+
+  test "greets the world" do
+    assert Pixir.hello() == :world
+  end
+end
